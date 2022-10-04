130c2ea227d0044ee79bced39f717ffce002737f
Martijn Vos <info@vosdevelopment.nl>
2022-10-04 21:37:54 +0200

    nvim: move diagnostic keymaps outside lsp config

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,9 +1,15 @@
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-if package.loaded['cmp_nvim_lsp'] then
-    capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+if package.loaded["cmp_nvim_lsp"] then
+    capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 end
+
+-- Vim Diagnostics
+vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {buffer=0})
+vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
+
+vim.keymap.set("n", "<leader>dl", vim.diagnostic.open_float)
 
 -- C#
 require'lspconfig'.csharp_ls.setup{
@@ -15,13 +21,9 @@ require'lspconfig'.csharp_ls.setup{
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
         vim.keymap.set("n", "gr", vim.lsp.buf.references, {buffer=0})
 
-        vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {buffer=0})
-        vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
-
         -- Dialogs
         vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
         vim.keymap.set("n", "<c-s>", vim.lsp.buf.signature_help, {buffer=0})
-        vim.keymap.set('n', "<leader>dl", vim.diagnostic.open_float)
 
         -- Options
         vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {buffer=0})
