95fa8f99c97d2fda46b54cb3667a7764d442f324
Martijn Vos <martijn.vos@cm.com>
2025-03-02 22:17:24 +0100

    Remove Neovim config in favor of https://github.com/Martijnvos/config.nvim

--- .config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.statusline"
-require "user.colorscheme"
-require "user.lsp"
-require "user.autocomplete"
-require "user.autocommands"
